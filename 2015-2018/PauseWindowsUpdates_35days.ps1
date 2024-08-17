50d13f76e3c1c449438d0d5dec588701d2f02393
Paul Fregonese <paul.fregonese@gmail.com>
2024-08-17 14:59:01 -0400

    Update all folders and commit changes

---
+++ 2015-2018/PauseWindowsUpdates_35days.ps1
@@ -0,0 +1,7 @@
+﻿$pause = (Get-Date).AddDays(35) 
+$pause = $pause.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ssZ" ) 
+Set-ItemProperty -Path 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseUpdatesExpiryTime' -Value $pause
+Set-ItemProperty -Path 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseFeatureUpdatesEndTime' -Value $pause
+Set-ItemProperty -Path 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseQualityUpdatesEndTime' -Value $pause
+Set-ItemProperty -Path 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseQualityUpdatesStartTime' -Value $pause
+Set-ItemProperty -Path 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseFeatureUpdatesStartTime' -Value $pause
